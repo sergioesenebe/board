d26f46697dd71b618f13b71f0a8c82c4b0b1ee5e
sergioesenebe <sergio.nunez@esenebe.com>
2025-03-23 20:47:39 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,13 +6,6 @@ $user = getenv('DB_USER');
 $password = getenv('DB_PASSWORD');
 $port = getenv('DB_PORT');
 
-// Mostrar las variables de entorno
-echo "<h1>Conexión a la base de datos</h1>";
-echo "<p><strong>Host:</strong> $host</p>";
-echo "<p><strong>Nombre de la base de datos:</strong> $dbname</p>";
-echo "<p><strong>Usuario:</strong> $user</p>";
-echo "<p><strong>Puerto:</strong> $port</p>";
-
 try {
     // Intentar conectar a la base de datos usando las variables de entorno
     $conexion = mysqli_connect($host,$user,$password,$dbname);
