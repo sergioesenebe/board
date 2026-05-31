847ac1d68508a4c1dd1a88add29d48a239f38d75
sergioesenebe <sergio.nunez@esenebe.com>
2026-05-31 12:40:12 +0200

    Use environment variable for backend port

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mysql = require('mysql2');
 const app = express();
-const port = 8001;
+const port = env.BACKEND_PORT;
 // Create a MySQL connection pool using environment variables
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
