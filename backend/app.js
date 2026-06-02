112dbb3aa56b911215f1014e2c4dcd8046909844
sergioesenebe <sergio.nunez@esenebe.com>
2026-06-02 10:25:16 +0200

    Change MySQL host to a static value

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 8001;
 // Create a MySQL connection pool using environment variables
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
+  host: mysql,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
